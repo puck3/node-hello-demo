ae83e265751166811776ff2560f72102c8a50f9c
Kirill Shipilov <shipilovk04@gmail.com>
2026-05-24 18:52:50 +0300

    deploy version 29

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const escape = require("lodash/escape");
 const morgan = require("morgan");
 const { z } = require("zod");
 
-const APP_VERSION = 28;
+const APP_VERSION = 29;
 
 const envSchema = z.object({
   PORT: z.coerce.number().int().positive().default(8080),
